9f9aa60d395c8d04548334222a24c529c34a1b74
Niiui <116442570+Niiui@users.noreply.github.com>
2023-07-26 19:03:39 +0800

    Update ringer.v

--- Circuits/Combinational Logic/Basic Gates/ringer.v
+++ Circuits/Combinational Logic/Basic Gates/ringer.v
@@ -5,7 +5,7 @@ module top_module (
     output motor         // Vibrate
 );
 
-    assign motor = (vibrate_mode & ring) ? 1 : 0;
-    assign ringer = (vibrate_mode) ? 0 : ((ring) ? 1 : 0);
+    assign motor = (vibrate_mode & ring) ? 1'b1 : 1'b0;
+    assign ringer = (vibrate_mode) ? 1'b0 : ((ring) ? 1'b1 : 1'b0);
 
 endmodule
