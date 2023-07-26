1cb0a37237f0fda008ee17ebe73c8734e17be2f7
Niiui <116442570+Niiui@users.noreply.github.com>
2023-07-26 16:27:04 +0800

    Create bcdadd100_2.v

---
+++ Verilog Language/More Verilog Features/bcdadd100_2.v
@@ -0,0 +1,13 @@
+module top_module( 
+    input [399:0] a, b,
+    input cin,
+    output cout,
+    output [399:0] sum );
+
+    wire [100:0] c;
+    assign c[0] = cin;
+
+    bcd_fadd obj[99:0] (a, b, c[99:0], c[100:1], sum);
+    assign cout = c[100];
+    
+endmodule
