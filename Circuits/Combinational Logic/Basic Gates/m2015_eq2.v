01d74387fd74dd7aac872f36472b19355516c2d0
Niiui <116442570+Niiui@users.noreply.github.com>
2023-07-26 17:20:48 +0800

    Update m2015_eq2.v

--- Circuits/Combinational Logic/Basic Gates/m2015_eq2.v
+++ Circuits/Combinational Logic/Basic Gates/m2015_eq2.v
@@ -1,5 +1,6 @@
 module top_module ( input [1:0] A, input [1:0] B, output z ); 
 
-  assign z = (A == B) ? 1'b1 : 1'b0;
+    assign z = (A == B) ? 1'b1 : 1'b0;
+    //assign z = (A[1:0] == B[1:0]);
 
 endmodule
