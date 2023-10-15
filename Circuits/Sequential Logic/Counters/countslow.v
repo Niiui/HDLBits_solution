f2be3f8965e0ad65c7199a7c865dd5b1428984fc
Niiui <116442570+Niiui@users.noreply.github.com>
2023-10-15 10:36:20 +0800

    Create countslow.v

---
+++ Circuits/Sequential Logic/Counters/countslow.v
@@ -0,0 +1,20 @@
+module top_module (
+    input clk,
+    input slowena,
+    input reset,
+    output [3:0] q);
+
+    always@(posedge clk) begin
+        if (reset)
+            q <= 'b0;
+        else if (slowena) begin
+            if (q=='d9)
+                q <= 'b0;
+            else
+                q <= q + 1'b1;
+        end
+        else
+            q <= q;
+    end
+
+endmodule
